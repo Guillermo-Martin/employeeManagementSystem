04715a6d288c2d8294d009b86023acc82a7cc15f
gcmartin <gscalica@gmail.com>
2020-04-24 18:01:58 -0700

    Add basic inquirer prompt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // require mysql and inquirer
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const cTable = require('console.table');
 
 // estalish connection with the database
 const connection = mysql.createConnection({
@@ -10,3 +11,35 @@ const connection = mysql.createConnection({
   database: 'employee_db',
 });
 
+// ===== INQUIRER QUESTIONS =====
+
+// Inquirer opening question
+const basicQuestion = [
+  {
+    type: 'list',
+    name: 'basic',
+    message: 'What would you like to do?',
+    choices: [
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'View a department',
+      'View a role',
+      'View an employee',
+      'Update an employee role',
+    ],
+  },
+];
+
+// ==============================
+
+
+// Prompt the user
+function askUser() {
+  // ask the user what they want to do
+  inquirer.prompt(basicQuestion).then(function(response) {
+    console.log(response);
+  });
+}
+
+askUser();
